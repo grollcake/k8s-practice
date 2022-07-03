adfda7ad2a98d406d4b512be1f172736190d258a
grollcake <grollcake@gmail.com>
2022-07-03 17:18:34 +0900

    OK

--- p9-persistent-volume/quick-start.sh
+++ p9-persistent-volume/quick-start.sh
@@ -23,14 +23,14 @@ cd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 kubectl apply -f k8s/apache-with-pv.yml
 
 # 상태 조회
-watch kubectl get all,pv,pvc -o wide -n apache
+kubectl get all,pv,pvc -o wide -n apache
 
 ##########################################################
 # Step 2. nginx에 PV와 subpath를 생성
 ##########################################################
 
 # pv, pvc, apache pod 생성 
-kubectl apply -f k8s/ngix-with-pv-subpath.yml
+kubectl apply -f k8s/nginx-with-pv-subpath.yml
 
 # 상태 조회
-watch kubectl get all,pv,pvc -o wide -n ngix
+kubectl get all,pv,pvc -o wide -n ngix
